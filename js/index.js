f0386fa66319dfefd38c8258fa30910abb6479e4
PPetja <petja-pal@i.ua>
2022-02-28 19:34:51 +0200

    adding thumbnail to the product page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
+// category page
 const showCardButton = document.querySelector('.content__button-show-more');
 const visibleCard = document.querySelector('.content__hover-card');
 showCardButton.addEventListener("click", function (event) {
     visibleCard.style.display = 'block';
     showCardButton.style.display = 'none';
 });
+
+// product page 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("slide-thumbnail");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      // slides[i].style.display = "inline";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  // slides[slideIndex-1].style.display = "inline";
+  dots[slideIndex-1].className += " active";
+}
