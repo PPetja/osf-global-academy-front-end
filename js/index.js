b89bbc95ccd82099e3c67ff6becd444cc0ae56b6
PPetja <petja-pal@i.ua>
2022-03-08 22:28:20 +0200

    Adding prettier formatting to code

--- js/index.js
+++ js/index.js
@@ -1,38 +1,42 @@
 // category page
-const showCardButton = document.querySelector('.content__button-show-more');
-const visibleCard = document.querySelector('.content__hover-card');
+const showCardButton = document.querySelector(".content__button-show-more");
+const visibleCard = document.querySelector(".content__hover-card");
 showCardButton.addEventListener("click", function (event) {
-    visibleCard.style.display = 'block';
-    showCardButton.style.display = 'none';
+  visibleCard.style.display = "block";
+  showCardButton.style.display = "none";
 });
 
-// product page 
+// product page
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
   var dots = document.getElementsByClassName("slide-thumbnail");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      // slides[i].style.display = "inline";
+    slides[i].style.display = "none";
+    // slides[i].style.display = "inline";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
   // slides[slideIndex-1].style.display = "inline";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex - 1].className += " active";
 }
