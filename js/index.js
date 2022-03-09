ffb214b6d7022971ee49128145548109d5f3c24a
PPetja <petja-pal@i.ua>
2022-03-09 23:53:06 +0200

    creating thumbnails with jquery

--- js/index.js
+++ js/index.js
@@ -7,36 +7,11 @@ showCardButton.addEventListener("click", function (event) {
 });
 
 // product page
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("slide-thumbnail");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    // slides[i].style.display = "inline";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  // slides[slideIndex-1].style.display = "inline";
-  dots[slideIndex - 1].className += " active";
-}
+$(".thumbnail").on("click", function () {
+  let clicked = $(this);
+  let newSelection = clicked.data("big");
+  let $img = $(".primary").css("background-image", "url(" + newSelection + ")");
+  clicked.parent().find(".thumbnail").removeClass("selected");
+  clicked.addClass("selected");
+  $(".primary").empty().append($img.hide().fadeIn("slow"));
+});
