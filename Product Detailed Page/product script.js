4479064df1b674246019e9dc9cf6851604f1d41e
PPetja <petja-pal@i.ua>
2022-03-24 00:21:15 +0200

    adding js for btn"Show more", social link

--- Product Detailed Page/product script.js
+++ Product Detailed Page/product script.js
@@ -20,17 +20,31 @@ $(".image-section__dropdown-list-container").on("click", function () {
 
 // Button with quantity the item
 const quantityText = $(".image-section__quantity-text");
-let counter = 0;  
+let counter = 0;
 
 $(".image-section__quantity-button-plus").on("click", function () {
-counter += 1;
-quantityText.empty().append(counter);
+  counter += 1;
+  quantityText.empty().append(counter);
 });
 
 $(".image-section__quantity-button-minus").on("click", function () {
   if (counter >= 1) {
     counter -= 1;
     quantityText.empty().append(counter);
+  } else {
   }
-  else{}
+});
+
+// text with describe
+
+$(".image-section__describe-button").on("click", function () {
+  const describeText = $(".image-section__describe-text");
+  
+  if (describeText.css("display") == "none") {
+    describeText.css("display", "inline");
+    $(this).empty().append("Read less");
+  } else {
+    describeText.css("display", "none");
+    $(this).empty().append("Read more");
+  }
 });
