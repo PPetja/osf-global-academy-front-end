b88f81f7d3559ec2cc6fb942c6b16da68dab461a
PPetja <petja-pal@i.ua>
2022-03-30 17:52:07 +0300

    Adding show img-button for password on the login

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// modal for cookies
 setTimeout(function () {
   $(".cookies__moadl").css("display", "block");
 }, 3000);
@@ -6,4 +7,12 @@ $(".cookies__modal-dialog-button-close").on("click", function () {
 });
 $(".cookies__modal-dialog-button-accept").on("click", function () {
   $(".cookies__moadl").css("display", "none");
+});
+
+// modal for login
+$(".login-modal__img-container").mousedown(function () {
+  $(".login-modal__form-password").removeAttr("type");
+});
+$(".login-modal__img-container").mouseup(function () {
+  $(".login-modal__form-password").attr("type", "password");
 });
