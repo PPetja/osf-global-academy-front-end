9d1a34270f42e7ed7732305269757731d29a2d29
PPetja <petja-pal@i.ua>
2022-03-31 14:48:16 +0300

    adding functional for bag and like counter

--- index.js
+++ index.js
@@ -17,13 +17,25 @@ $(function () {
   $(".login-modal__img-container").mouseup(function () {
     $(".login-modal__form-password").attr("type", "password");
   });
+  // sesction 1
+
+  let counterBag = 0;
+  let counterlike = 0;
 
   $(".card__hover-btn-bag").on("click", function () {
+    counterBag += 1;
     $(".counter-bag").css("visibility", "visible").empty();
-    $(".counter-bag").append(1);
+    $(".counter-bag").append(counterBag);
+    if (counterBag >= 10) {
+      $(".counter-bag").css("padding-top", "2px").css("padding-bottom", "2px");
+    }
   });
   $(".card__hover-btn-like").on("click", function () {
+    counterlike += 1;
     $(".counter-like").css("visibility", "visible").empty();
-    $(".counter-like").append(1);
+    $(".counter-like").append(counterlike);
+    if (counterlike >= 10) {
+      $(".counter-like").css("padding-top", "2px").css("padding-bottom", "2px");
+    }
   });
 });
